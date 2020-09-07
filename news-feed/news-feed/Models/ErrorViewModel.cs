d72f377cb268003bb5c57212a07d94d5bd50f0c1
Yan Alves <yanalves@hotmail.com>
2020-09-07 17:00:38 -0300

    :rocket: Initial solution configuration

---
+++ news-feed/news-feed/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace news_feed.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

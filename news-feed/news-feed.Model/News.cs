bfa75dce9830dbfe6c24689bd41baeb19d8541eb
Yan Alves <yanalves@hotmail.com>
2020-09-07 17:09:56 -0300

    :construction: Add News and NewsFeed models

---
+++ news-feed/news-feed.Model/News.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace news_feed.Model
+{
+    public class News
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ news-feed/news-feed.Model/NewsFeed.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace news_feed.Model
+{
+    public class NewsFeed
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<News> News { get; set; }
+    }
+}
